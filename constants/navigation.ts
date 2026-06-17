47f6a141eb543cbf6b7a795d9cdbf2c87bf87fe7
sonali0125 <sonalisaratkar11@gmail.com>
2026-06-17 12:33:31 +0530

    Add files via upload

---
+++ constants/navigation.ts
@@ -0,0 +1,38 @@
+export const navigation = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "Jewellery",
+    href: "/jewellery",
+  },
+  {
+    title: "Collections",
+    href: "/collections",
+  },
+  {
+    title: "Wedding",
+    href: "/wedding",
+  },
+  {
+    title: "Gold",
+    href: "/gold",
+  },
+  {
+    title: "Diamond",
+    href: "/diamond",
+  },
+  {
+    title: "Gifting",
+    href: "/gifting",
+  },
+  {
+    title: "About",
+    href: "/about",
+  },
+  {
+    title: "Contact",
+    href: "/contact",
+  },
+];
